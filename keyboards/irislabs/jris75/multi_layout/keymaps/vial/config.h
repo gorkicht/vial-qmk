985693bbb631fd6ee38388ab8d3b43d4702ee3cf
gorkicht <156505360+gorkicht@users.noreply.github.com>
2024-01-16 21:59:54 +0100

    first release

---
+++ keyboards/irislabs/jris75/multi_layout/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x85, 0x40, 0x30, 0xBD, 0xC4, 0xC7, 0x64, 0xB0}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
+#define VIAL_COMBO_ENTRIES 64
+#define DYNAMIC_KEYMAP_MACRO_COUNT 64
+
